3445f921894ea12beda4fefb6306ae35ce5def9d
Murtuza Rahman <murtuza.rahman83@gmail.com>
2023-09-04 15:10:29 +0600

    added json parse

--- restApi.cpp
+++ restApi.cpp
@@ -3,10 +3,23 @@
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
 #include <mutex>
+#include <string>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
 
 namespace asio = boost::asio;
 namespace beast = boost::beast;
 using tcp = boost::asio::ip::tcp;
+namespace pt = boost::property_tree;
+
+
+// Define a C++ model class that mirrors the JSON structure
+struct MyModel {
+    std::string title;
+    std::string body;
+    int userId;
+    int id;
+};
 
 class NetworkManager {
 public:
@@ -102,7 +115,7 @@ int main() {
         NetworkManager networkManager(baseUrl);
 
         #pragma omp parallel for
-        for (int i = 0; i < 20; ++i) {
+        for (int i = 0; i < 1; ++i) {
             std::string postRequestBody = R"({
                 "title": "foo",
                 "body": "bar",
@@ -113,6 +126,55 @@ int main() {
             #pragma omp critical
             {
                 std::cout << "Response from iteration " << i << ":\n" << response << std::endl;
+
+                // Create an empty property tree
+                boost::property_tree::ptree pt;
+
+                // Load the JSON data into the property tree
+                std::istringstream jsonStream(response);
+                boost::property_tree::read_json(jsonStream, pt);
+
+                // Check if "title" key exists
+                boost::optional<std::string> title1 = pt.get_optional<std::string>("title");
+                
+                if (title1) {
+                    std::cout << "Title exists: " << *title1 << std::endl;
+                } else {
+                    std::cout << "Title does not exist." << std::endl;
+                }
+
+                // Access individual elements in the JSON
+                std::string title = pt.get<std::string>("title");
+                std::string body = pt.get<std::string>("body");
+                int userId = pt.get<int>("userId");
+                int id = pt.get<int>("id");
+
+                // Print the parsed values
+                std::cout << "Title: " << title << std::endl;
+                std::cout << "Body: " << body << std::endl;
+                std::cout << "User ID: " << userId << std::endl;
+                std::cout << "ID: " << id << std::endl;
+
+                // another way
+                std::cout << "=====another way====" << std::endl;
+                pt::ptree jsonTree;
+    
+                std::istringstream jsonStream2(response);
+                pt::read_json(jsonStream2, jsonTree);
+
+                // Create an instance of the model class and populate it from the property tree
+                MyModel model;
+                model.title = jsonTree.get<std::string>("title");
+                model.body = jsonTree.get<std::string>("body");
+                model.userId = jsonTree.get<int>("userId");
+                model.id = jsonTree.get<int>("id");
+
+                // Access and print the model's attributes
+                std::cout << "Title: " << model.title << std::endl;
+                std::cout << "Body: " << model.body << std::endl;
+                std::cout << "User ID: " << model.userId << std::endl;
+                std::cout << "ID: " << model.id << std::endl;
+
             }
         }
     } catch (std::exception const& e) {
